db402590a300a779db063e5588fc10d0537c02c0
n19013 <n19013@std.it-college.ac.jp>
2020-01-15 15:25:56 +0900

    react

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const Hello = props => {}
+const Hello = props => {
+  return <h1>Hello world</h1>
+}
 
 class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {}
   }
-
   render () {
     return (
       <div>
