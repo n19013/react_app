40d6cab4f3ff28050c313355467b522aea0ed70c
n19013 <n19013@std.it-college.ac.jp>
2020-01-15 14:35:06 +0900

    react

---
+++ react_app/react-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hello = props => {
+  return <h1>Hello world</h1>
+}
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+  render () {
+    return (
+      <div>
+        <Hello />
+      </div>
+    )
+  }
+}
+
+export default App
